7df73f6c68e1a19da06d8a2da4c586c9600f7638
Matheus <matheusfrotacsc@edu.unifor.br>
2026-01-08 00:37:20 -0300

    style: remove topbar

--- src/layouts/SiteLayout.tsx
+++ src/layouts/SiteLayout.tsx
@@ -8,7 +8,7 @@ const SiteLayout: React.FC = () => {
   return (
     <div className="flex min-h-screen flex-col bg-brand-sand">
       <TopBar />
-      <main className="flex-1 pt-28 md:pt-32">
+      <main className="flex-1 pt-20 md:pt-24">
         <Outlet />
       </main>
       <CartDrawer />
